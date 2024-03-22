484902b95ae00b912d9b23b6042465939ad266ea
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-22 13:34:34 +0000

    new commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Creates a file in /tmp.
 
 file { 'school':
+  content => 'I love Puppet'
   path    => '/tmp/school'
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
 }
