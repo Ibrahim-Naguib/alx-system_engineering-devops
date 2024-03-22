1418903c8849e7b6646ee6bdff231a04b6dacf96
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-22 15:03:44 +0000

    new commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,9 @@
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3'
-  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => installed,
+  provider => 'pip3',
 }
