8a56de8e600886840705640d7c2145c41f057f3e
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-24 14:49:17 +0000

    new commit

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,5 @@
+# Makes changes to the configuration file
+exec { 'echo':
+  path    => '/usr/bin:/bin',
+  command => 'echo "	IdentityFile ~/.ssh/school\n	PasswordAuthentication no" >> /etc/ssh/ssh_config',
+}
