21cfe2394f219a2d3909772e83a351f323c3776c
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-26 15:56:58 +0000

    new commit

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,28 @@
-# Script that installs Nginx with Puppet
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx ;'
+# Install Nginx
+package { 'nginx':
+  ensure  => installed,
 }
 
-exec {'echo':
-  path    => '/usr/bin:/bin',
-  command => 'echo "Hello World!" > /var/www/html/index.html'
+exec { 'install_nginx':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  path     => '/usr/bin:/bin',
 }
 
-exec {'echo':
-  path    => '/usr/bin:/bin',
-  command => 'echo "Ceci n\'est pas une page" > /var/www/html/404.html'
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
 }
 
-exec {'echo':
+file { '/var/www/html/404.html':
+  ensure  => file,
+  content => 'Ceci n\'est pas une page',
+}
+
+exec { 'echo':
   path    => '/usr/bin:/bin',
-  command => 'echo "server {
-    listen 80;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
-    location /redirect_me {
-        return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-    error_page 404 /404.html;
-    location /404.html {
-        root /var/www/html;
-        internal;
-    }
-  }" > /etc/nginx/sites-available/default'
+  command => 'echo "server { listen 80; listen [::]:80 default_server; root /var/www/html; index index.html index.htm index.nginx-debian.html; server_name _; location /redirect_me { return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4; } error_page 404 /404.html; location /404.html { root /var/www/html; internal; } }" > /etc/nginx/sites-available/default'
 }
 
 service { 'nginx':
   ensure  => running,
-  require => Package['nginx'],
 }
