0a6df8b57e21ecfe7ae6ffd5c231c634cf54b301
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-03-30 23:41:01 +0000

    new commit

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+# Installs Nginx server with custome HTTP header
+
+package { 'nginx':
+  ensure  => installed,
+}
+
+exec { 'install_nginx':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  path     => '/usr/bin:/bin',
+}
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+}
+
+service { 'nginx':
+  ensure  => running,
+}
