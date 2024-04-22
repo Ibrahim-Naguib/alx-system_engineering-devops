281d9b72c50876318c22d9ba5256463d26725077
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-22 15:26:06 +0000

    new commit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     user = requests.get(url + "/users/{}".format(user_id)).json()
     username = user.get("username")
     todos = requests.get(url + "/todos", params={"userId": user_id}).json()
-                   
+
     completed = []
     for todo in todos:
         if todo.get("completed") is True:
