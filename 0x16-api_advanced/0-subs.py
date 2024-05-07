16fdc23872ae4acd753ab8e03b18b6926adbe17f
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-05-07 14:30:13 +0000

    new commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,6 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Retrieve the number of subs for a given subreddit from the Reddit API
-
-    Parameters:
-    - subreddit (str): The name of the subreddit to query.
-
-    Returns:
-    - int: The number of subscribers of the specified subreddit or
-           0 if not a valid subreddit.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 \
