b5303caea921939bf7ef8e956211832fc43b6540
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-05-07 14:28:51 +0000

    new commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -21,8 +21,7 @@ def number_of_subscribers(subreddit):
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        results = response.json().get("data")
-        return results.get("subscribers")
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
