36577d8287ab3c80f31aa9e13107cd52ec8d08aa
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-05-07 14:33:32 +0000

    new commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,16 +5,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Retrieve the number of subs for a given subreddit from the Reddit API
-    """
+    """Retrieve the number of subs"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 \
-                              (by /u/Tiny-Poetry-2335)"}
+    headers = {'User-Agent': 'My User Agent 1.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 404:
+    if response.status_code == 200:
+        results = response.json()
+        return results["data"]["subscribers"]
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
